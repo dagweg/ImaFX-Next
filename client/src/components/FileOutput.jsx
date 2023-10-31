1eb67669d08629c73dd316b17f386623dfe71a65
dagweg <dagtef2@gmail.com>
2023-10-31 18:09:27 +0300

    waiting 0_o

--- client/src/components/FileOutput.jsx
+++ client/src/components/FileOutput.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/fileOutput.css';
 import { Waveform } from "@uiball/loaders"
+import { Ripples } from '@uiball/loaders'
+
 
 
 function FileOutput({ imageURL, setImageURL }) {
@@ -18,7 +20,12 @@ function FileOutput({ imageURL, setImageURL }) {
             <div className='file-output-wrapper flex justify-center align-middle p-2 m-2 max-w-lg relative'>
                 {imageURL && <i className='fa-solid fa-close absolute top-0 right-0 -mt-4 -mr-4 scale-150 p-1 transition-all duration-150 rounded-full transform hover:bg-gray-100 cursor-pointer' onClick={() => setImageURL('')}></i>}
                 <img src={imageURL} alt='' className='max-w-full max-h-full' onChange={imageLoaded} />
-                {loader.visible && <Waveform size={40} lineWeight={3.5} speed={1} color="black" />}
+                {loader.visible && (
+                    <>
+                        <Ripples size={45} speed={2} color="black" />
+                        waiting for input
+                    </>
+                )}
             </div>
         </>
     );
