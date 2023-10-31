fcd5255674a3a7b915303a9a6c772951442b6d3b
dagweg <dagtef2@gmail.com>
2023-10-31 18:05:01 +0300

    added close and tested waveload

--- client/src/components/FileOutput.jsx
+++ client/src/components/FileOutput.jsx
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import '../styles/fileOutput.css';
+import { Waveform } from "@uiball/loaders"
 
 
-function FileOutput({ imageURL }) {
+function FileOutput({ imageURL, setImageURL }) {
+
+    const [loader, setLoader] = useState({ visible: true })
+
+    const imageLoaded = () => {
+
+        setLoader((prev) => ({ ...prev, visible: true }))
+    }
+
+
     return (
         <>
-            <div className='file-output-wrapper flex justify-center align-middle p-2 m-2 max-w-lg'>
-                <img src={imageURL} alt='' className='max-w-full max-h-full' />
+            <div className='file-output-wrapper flex justify-center align-middle p-2 m-2 max-w-lg relative'>
+                {imageURL && <i className='fa-solid fa-close absolute top-0 right-0 -mt-4 -mr-4 scale-150 p-1 transition-all duration-150 rounded-full transform hover:bg-gray-100 cursor-pointer' onClick={() => setImageURL('')}></i>}
+                <img src={imageURL} alt='' className='max-w-full max-h-full' onChange={imageLoaded} />
+                {loader.visible && <Waveform size={40} lineWeight={3.5} speed={1} color="black" />}
             </div>
         </>
     );
