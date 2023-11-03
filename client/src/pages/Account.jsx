aaa68d7d2893617e075d4273c3ea14e2cd1eed6a
dagweg <dagtef2@gmail.com>
2023-11-03 08:03:55 +0300

    added accounts page

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import '../styles/account.css'
 
 function Account() {
+
+    const accountPageMenuItems = [
+        "Dashboard",
+        "Profile",
+        "Security",
+        "Privacy Settings",
+        "Notifications",
+        "Preferences",
+        "Payment",
+        "Orders/History",
+        "Support/Help Center",
+        "Log Out",
+        "Delete Account",
+        "Terms and Privacy Policy",
+    ];
+
     return (
-        <div>Account</div>
+        <>
+            <div className='flex w-full h-full relative'>
+                <nav className='side-bar bg-gray-200 relative '>
+                    <h1 className='title text-4xl font-black text-gray-1000 py-8 bg-gray-300 text-center'>Account Settings</h1>
+                    <ul className='flex flex-col justify-center overflow-scroll h-full'>
+                        {accountPageMenuItems.map((menu, index) => (
+                            <li className='m-1 py-5 cursor-pointer hover:bg-gray-50 pl-40' key={index}><h1>{menu}</h1></li>
+                        ))}
+                    </ul>
+                </nav>
+                <div className='main-bar bg-gray-300 relative'>
+                </div>
+            </div>
+        </>
     )
 }
 
