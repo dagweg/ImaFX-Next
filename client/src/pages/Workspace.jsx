9e0e542124aa24cdaeb2fa4b4f25d837e8cc8d14
dagweg <dagtef2@gmail.com>
2023-11-05 22:08:24 +0300

    added modifiers and effectHandlers

--- client/src/pages/Workspace.jsx
+++ client/src/pages/Workspace.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useRef, useEffect } from 'react'
 import imageProcessingEffects from '../utilities/effects';
-import { grayscale } from '../utilities/modifiers';
 import '../styles/workspace.css'
 import { grid } from 'ldrs'
 
@@ -20,7 +19,6 @@ function Workspace() {
             id: i
         }))
         setDropDownMenu(effects)
-        console.log(dropDownMenu)
 
         grid.register() // registering the loader 
     }, [])
@@ -30,30 +28,6 @@ function Workspace() {
         setTimeout(() => setIsLoading(prev => false), 2000)
     }, [imageURL])
 
-    const handleFileChange = (e) => {
-        const file = e.target.files[0]
-        if (file) {
-            setImageURL(URL.createObjectURL(file))
-        }
-    }
-
-    const addImageClicked = () => {
-        fileInput.current.click()
-    }
-
-    const dropDownMenuClick = (index) => {
-        const updated = dropDownMenu.map((obj, i) => {
-            if (i === index) {
-                return ({
-                    ...obj,
-                    hidden: obj.hidden ? false : true
-                })
-            }
-            return obj
-        })
-        setDropDownMenu(prev => updated)
-    }
-
 
     return (
         <section className='relative flex flex-col justify-center h-screen' style={{ fontSize: 15 }}>
@@ -61,28 +35,7 @@ function Workspace() {
                 <section className='modifier-pane overflow-y-scroll'>
                     <h1>Modifiers</h1>
                     <div className='relative'>
-                        {dropDownMenu.map((obj, i) => (
-                            <div>
-                                <div key={i} className='dropdown-menu' onClick={() => dropDownMenuClick(i)}>
-                                    <label htmlFor="">{obj.category}</label>
-                                    <i className={`fa-solid ${!obj.hidden ? 'fa-angle-up' : 'fa-angle-down'}`}></i>
-                                </div>
-
-                                {
-                                    !obj.hidden &&
-                                    <div className={`dropdown-menu-buttons-container`} key={i} ref={dropDownBtnContainer}>
-
-                                        {obj.effects.map((effect, j) => (
-                                            <div key={j} className='dropdown-menu-button' onClick={() => console.log(effect, j)}>
-                                                <label htmlFor="">{effect}</label>
-                                                <i className='fa-solid '></i>
-                                            </div>
-                                        ))}
-                                    </div>
-                                }
-
-                            </div>
-                        ))}
+                        <DropDownMenu dropDownMenu={dropDownMenu} setDropDownMenu={setDropDownMenu} dropDownBtnContainer={dropDownBtnContainer} />
                     </div>
                 </section >
                 <section className='canvas'>
@@ -92,29 +45,9 @@ function Workspace() {
                     </div>
                     <div className='canvas-main'>
                         {
-                            imageURL !== '' ? (
-                                isLoading ?
-                                    <>
-                                        <l-grid size={"120"} speed={"1.5"} color={"black"}></l-grid>
-                                        <br></br>
-                                        <p>Loading file...</p>
-                                    </>
-                                    :
-                                    <>
-                                        <img src={`${imageURL}`} alt='' className='image'></img>
-
-                                    </>
-                            )
-                                :
-                                <div className='add-image-wrapper' onClick={addImageClicked}>
-                                    <input type="file" ref={fileInput} onChange={handleFileChange} hidden />
-                                    <div className="icon">
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24"><g strokeWidth="0" id="SVGRepo_bgCarrier"></g><g strokeLinejoin="round" strokeLinecap="round" id="SVGRepo_tracerCarrier"></g><g id="SVGRepo_iconCarrier"> <path fill="" d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z" clipRule="evenodd" fillRule="evenodd"></path> </g></svg>
-                                    </div>
-                                    <div className="text">
-                                        <span>Click to upload image</span>
-                                    </div>
-                                </div>
+                            imageURL !== '' ?
+                                (isLoading ? <Loader /> : <Image imageURL={imageURL} />)
+                                : <AddImageComponent fileInput={fileInput} setImageURL={setImageURL} />
                         }
                     </div>
                 </section>
@@ -122,5 +55,97 @@ function Workspace() {
         </section >
     )
 }
-
 export default Workspace
+
+
+const DropDownMenu = ({ dropDownMenu, setDropDownMenu, dropDownBtnContainer }) => {
+
+    return (
+        <>
+            {
+                dropDownMenu.map((obj, i) => (
+                    <div>
+                        <div key={i} className='dropdown-menu' onClick={() => dropDownMenuClick(i, dropDownMenu, setDropDownMenu)}>
+                            <label htmlFor="">{obj.category}</label>
+                            <i className={`fa-solid ${!obj.hidden ? 'fa-angle-up' : 'fa-angle-down'}`}></i>
+                        </div>
+
+                        {
+                            !obj.hidden &&
+                            <div className={`dropdown-menu-buttons-container`} key={i} ref={dropDownBtnContainer}>
+
+                                {obj.effects.map((effect, j) => (
+                                    <div key={j} className='dropdown-menu-button'>
+                                        <label htmlFor="">{effect}</label>
+                                    </div>
+                                ))}
+                            </div>
+                        }
+
+                    </div>
+                ))
+            }
+        </>
+    )
+}
+
+
+const AddImageComponent = ({ fileInput, setImageURL }) => {
+    return (
+        <>
+            <div className='add-image-wrapper' onClick={() => addImageClicked(fileInput)}>
+                <input type="file" ref={fileInput} onChange={(e) => handleFileChange(e, setImageURL)} hidden />
+                <div className="icon">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="" viewBox="0 0 24 24"><g strokeWidth="0" id="SVGRepo_bgCarrier"></g><g strokeLinejoin="round" strokeLinecap="round" id="SVGRepo_tracerCarrier"></g><g id="SVGRepo_iconCarrier"> <path fill="" d="M10 1C9.73478 1 9.48043 1.10536 9.29289 1.29289L3.29289 7.29289C3.10536 7.48043 3 7.73478 3 8V20C3 21.6569 4.34315 23 6 23H7C7.55228 23 8 22.5523 8 22C8 21.4477 7.55228 21 7 21H6C5.44772 21 5 20.5523 5 20V9H10C10.5523 9 11 8.55228 11 8V3H18C18.5523 3 19 3.44772 19 4V9C19 9.55228 19.4477 10 20 10C20.5523 10 21 9.55228 21 9V4C21 2.34315 19.6569 1 18 1H10ZM9 7H6.41421L9 4.41421V7ZM14 15.5C14 14.1193 15.1193 13 16.5 13C17.8807 13 19 14.1193 19 15.5V16V17H20C21.1046 17 22 17.8954 22 19C22 20.1046 21.1046 21 20 21H13C11.8954 21 11 20.1046 11 19C11 17.8954 11.8954 17 13 17H14V16V15.5ZM16.5 11C14.142 11 12.2076 12.8136 12.0156 15.122C10.2825 15.5606 9 17.1305 9 19C9 21.2091 10.7909 23 13 23H20C22.2091 23 24 21.2091 24 19C24 17.1305 22.7175 15.5606 20.9844 15.122C20.7924 12.8136 18.858 11 16.5 11Z" clipRule="evenodd" fillRule="evenodd"></path> </g></svg>
+                </div>
+                <div className="text">
+                    <span>Click to upload image</span>
+                </div>
+            </div>
+        </>
+    )
+}
+
+
+const Loader = () => {
+    return (
+        <>
+            <l-grid size={"120"} speed={"1.5"} color={"black"}></l-grid>
+            <br></br>
+            <p>Loading file...</p>
+        </>
+    )
+}
+
+const Image = ({ imageURL }) => {
+    return (
+        <>
+            <img src={`${imageURL}`} alt='' className='image'></img>
+        </>
+    )
+}
+
+
+const handleFileChange = (e, setImageURL) => {
+    const file = e.target.files[0]
+    if (file) {
+        setImageURL(URL.createObjectURL(file))
+    }
+}
+
+const addImageClicked = (fileInput) => {
+    fileInput.current.click()
+}
+
+const dropDownMenuClick = (index, dropDownMenu, setDropDownMenu) => {
+    const updated = dropDownMenu.map((obj, i) => {
+        if (i === index) {
+            return ({
+                ...obj,
+                hidden: obj.hidden ? false : true
+            })
+        }
+        return obj
+    })
+    setDropDownMenu(prev => updated)
+}
--- client/src/utilities/modifiers.js
+++ client/src/utilities/modifiers.js
@@ -1,15 +1,91 @@
-export const grayscale = (imageURL) => {
+export function applyGrayscale(imageURL) { }
 
-    fetch('http://localhost:5000/modifier/grayscale', {
-        method: "POST",
-        body: {
-            url: imageURL
-        }
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data)
-            // setImageURL(data.url)
-        })
-        .catch((error) => { console.log(error) })
-}
+export function applySepia(imageURL) { }
+
+export function applyVintage(imageURL) { }
+
+export function applyBlackAndWhite(imageURL) { }
+
+export function applyCartoonize(imageURL) { }
+
+export function applyOilPainting(imageURL) { }
+
+export function applyWatercolor(imageURL) { }
+
+export function applyPopArt(imageURL) { }
+
+export function applyBlur(imageURL) { }
+
+export function applySharpen(imageURL) { }
+
+export function applyNoiseReduction(imageURL) { }
+
+export function applyBrightnessContrast(imageURL) { }
+
+export function applySaturation(imageURL) { }
+
+export function applyHueSaturation(imageURL) { }
+
+export function applyColorBalance(imageURL) { }
+
+export function applyColorize(imageURL) { }
+
+export function applyColorSplash(imageURL) { }
+
+export function applyCrop(imageURL) { }
+
+export function applyResize(imageURL) { }
+
+export function applyRotate(imageURL) { }
+
+export function applyFlip(imageURL) { }
+
+export function applyAddTextToImages(imageURL) { }
+
+export function applyWatermarking(imageURL) { }
+
+export function applyRedEyeRemoval(imageURL) { }
+
+export function applyBlemishRemoval(imageURL) { }
+
+export function applyWrinkleReduction(imageURL) { }
+
+export function applyTeethWhitening(imageURL) { }
+
+export function applyAutoEnhance(imageURL) { }
+
+export function applySharpenImage(imageURL) { }
+
+export function applyDenoiseImage(imageURL) { }
+
+export function applyAutoCorrect(imageURL) { }
+
+export function applyImpressionism(imageURL) { }
+
+export function applyPointillism(imageURL) { }
+
+export function applyCubism(imageURL) { }
+
+export function applyMosaic(imageURL) { }
+
+export function applyPixelation(imageURL) { }
+
+export function applyCreateImageCollages(imageURL) { }
+
+export function applyCombineMultipleImages(imageURL) { }
+
+export function applyDistortions(imageURL) { }
+
+export function applyPerspectiveCorrection(imageURL) { }
+
+export function applyTiltShift(imageURL) { }
+
+export function applyRemoveBackground(imageURL) { }
+
+export function applyAddDecorativeFrames(imageURL) { }
+
+export function applyAddBorders(imageURL) { }
+
+export function applyCompressAndSave(imageURL) { }
+
+export function applyCreateAnaglyph(imageURL) { }
